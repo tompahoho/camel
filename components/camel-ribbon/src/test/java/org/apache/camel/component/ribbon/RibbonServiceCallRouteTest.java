867eb8742b4e7708fce46e2e80e340fc7f6f3dbc
Claus Ibsen <davsclaus@apache.org>
2016-04-26 16:41:56 +0200

    CAMEL-9683: Started on camel-ribbon

---
+++ components/camel-ribbon/src/test/java/org/apache/camel/component/ribbon/RibbonServiceCallRouteTest.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.ribbon;
+
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.ServiceCallConfigurationDefinition;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore("Manual test")
+public class RibbonServiceCallRouteTest extends CamelTestSupport {
+
+    @Test
+    public void testServiceCall() throws Exception {
+        getMockEndpoint("mock:result").expectedMessageCount(1);
+
+        template.sendBody("direct:start", "Hello World");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                ServiceCallConfigurationDefinition config = new ServiceCallConfigurationDefinition();
+                config.setMasterUrl("https://fabric8-master.vagrant.f8:8443");
+                config.setUsername("admin");
+                config.setPassword("admin");
+                config.setNamespace("default");
+                // lets use the built-in round robin (random is default)
+                config.setLoadBalancerRef("roundrobin");
+
+                from("direct:start")
+                        .serviceCall("cdi-camel-jetty", null, config)
+                        .to("mock:result");
+            }
+        };
+    }
+}
+
