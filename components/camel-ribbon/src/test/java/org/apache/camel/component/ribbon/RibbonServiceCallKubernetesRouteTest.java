3efd93997946e3eaf93606d1dc5331ec849eded7
Claus Ibsen <davsclaus@apache.org>
2016-04-27 13:57:53 +0200

    CAMEL-9683: Started on camel-ribbon

--- components/camel-ribbon/src/test/java/org/apache/camel/component/ribbon/RibbonServiceCallKubernetesRouteTest.java
+++ components/camel-ribbon/src/test/java/org/apache/camel/component/ribbon/RibbonServiceCallKubernetesRouteTest.java
@@ -16,27 +16,21 @@
  */
 package org.apache.camel.component.ribbon;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.ServiceCallConfigurationDefinition;
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Ignore;
 import org.junit.Test;
 
-public class RibbonServiceCallRouteTest extends CamelTestSupport {
+@Ignore("Manual test")
+public class RibbonServiceCallKubernetesRouteTest extends CamelTestSupport {
 
     @Test
     public void testServiceCall() throws Exception {
-        getMockEndpoint("mock:9090").expectedMessageCount(1);
-        getMockEndpoint("mock:9091").expectedMessageCount(1);
-        getMockEndpoint("mock:result").expectedMessageCount(2);
+        getMockEndpoint("mock:result").expectedMessageCount(1);
 
-        String out = template.requestBody("direct:start", null, String.class);
-        String out2 = template.requestBody("direct:start", null, String.class);
-        assertEquals("9091", out);
-        assertEquals("9090", out2);
+        template.sendBody("direct:start", "Hello World");
 
         assertMockEndpointsSatisfied();
     }
@@ -46,29 +40,17 @@ public class RibbonServiceCallRouteTest extends CamelTestSupport {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                // setup a static ribbon server list
-                List<RibbonServer> servers = new ArrayList<>();
-                servers.add(new RibbonServer("localhost", 9090));
-                servers.add(new RibbonServer("localhost", 9091));
-                RibbonServiceCallServerListStrategy list = new RibbonServiceCallServerListStrategy(servers);
-
-
-
-                // configure camel service call
                 ServiceCallConfigurationDefinition config = new ServiceCallConfigurationDefinition();
-                config.setServerListStrategy(list);
+                config.setMasterUrl("https://fabric8-master.vagrant.f8:8443");
+                config.setUsername("admin");
+                config.setPassword("admin");
+                config.setNamespace("default");
+                // lets use the built-in round robin (random is default)
+                config.setLoadBalancerRef("roundrobin");
 
                 from("direct:start")
                         .serviceCall("cdi-camel-jetty", null, config)
                         .to("mock:result");
-
-                from("jetty:http://localhost:9090")
-                    .to("mock:9090")
-                    .transform().constant("9090");
-
-                from("jetty:http://localhost:9091")
-                    .to("mock:9091")
-                    .transform().constant("9091");
             }
         };
     }
